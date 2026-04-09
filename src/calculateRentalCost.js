ba60523de061d70e4e1bf6121ee6bc339d0c96f2
Yaroslav Volkov <olkov1979164@gmail.com>
2026-04-09 15:26:50 +0200

    added function calculateRentalCost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let res = 0;
+  const costCar = 40;
+  const cost = costCar * days;
+
+  if (days >= 7) {
+    res = cost - 50;
+  } else if (days >= 3) {
+    res = cost - 20;
+  } else {
+    res = cost;
+  }
+
+  return res;
 }
 
 module.exports = calculateRentalCost;
